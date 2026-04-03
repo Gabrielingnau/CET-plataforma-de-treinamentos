cbaecc5b974ac349c9258d62ef88602001fce025
Gabriel Lingnau <gabriellingnau@MacBook-Air-de-Gabriel.local>
2026-04-02 23:08:15 -0400

    aaaa

--- services/modules/create-module.ts
+++ services/modules/create-module.ts
@@ -1,7 +1,11 @@
 // services/modules/create-module.ts
 import { supabase } from "@/lib/supabase/client" // Usar sempre o client para Mutations em forms
 import { CreateModulePayload } from "@/types/database/modules"
-
+/*
+  Função para criar um novo módulo no banco de dados usando Supabase.
+  Recebe um objeto do tipo CreateModulePayload e insere na tabela "modules".
+  Retorna os dados do módulo criado ou lança um erro em caso de falha.
+*/
 export async function createModule(module: CreateModulePayload) {
   const { data, error } = await supabase
     .from("modules")
